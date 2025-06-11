b099663834d6b065dfdca766133dee747d7afecf
lalaholmesss <lalaholmesart00@gmail.com>
2025-06-12 01:11:35 +0400

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Hi im Hasan's pc`,
+    key: `A key to a secret dungeon`,
+    bed: `A bed for two hmmmmm ;)`,
+    table: `You should be able to sit here...`,
+    pcTable: `You can't sit here`,
+    cake: `Lala made this <span style="color:deeppink">C</span> <span style="color:chartreuse;">A</span> <span style="color:yellow">K</span> <span style="color:lightskyblue">E</span>`,
+    refrigerator: `There are a lot of fridge magnets and a grocery list <span style="font-size: 20px">ooops I forgot to draw them :D</span>`,
+    cutie: `Nunu is eeeeeeeping <span style="color:blue; font-size: 16px">Zzz</span>`,
+    pics: `<span style="font-size:3rem; color:deeppink;">HAPPY BIRTHDAY BEBIIIIIIIIIIII♥♥♥♥♥</span>`,
+    notes: `It says <span style="color:red">"I love you"</span> here`
+    
+}
